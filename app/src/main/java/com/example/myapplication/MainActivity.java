9b10bb58ccd30b99edb3a2e0e1189b7e845587bb
Leland Long <lelandlong@gmail.com>
2019-12-31 23:14:15 -0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onForumCLick(View v) {
+        Intent intent = new Intent(getBaseContext(), ForumPage.class);
+        startActivity(intent);
+    }
+
+}
